d2f8c74dad767de0239be8c9747dbd4579ffb53c
csliujw <695466632@qq.com>
2021-11-13 23:16:56 +0800

    add leetcode 多线程编程题

--- concurrent/src/main/java/leetcode/H2O_1_1117.java
+++ concurrent/src/main/java/leetcode/H2O_1_1117.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class H2O_1_1117 {
     static class H2O {
@@ -10,13 +11,20 @@ public class H2O_1_1117 {
 
         }
 
+        Semaphore H = new Semaphore(2);
+        Semaphore O = new Semaphore(0);
+
         // 两个 H 一个 O
         public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+            H.acquire();
             releaseHydrogen.run();
+            O.release();
         }
 
         public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+            O.acquire(2);
             releaseOxygen.run();
+            H.release(2);
         }
     }
 }
